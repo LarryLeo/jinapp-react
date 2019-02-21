c9f8cedf7d9142b7748482f39f8a0aa5c018a753
LarryLeo <esharemore@gmail.com>
2019-02-21 11:27:54 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React, { Component } from 'react'
+import { GlobalStyle } from './style'
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <GlobalStyle />
+        <p>Hello World</p>
+        <p>默认样式测试</p>
+        <ul>
+          <li>列表样式测试</li>
+          <li>列表样式测试</li>
+          <li>列表样式测试</li>
+          <li>列表样式测试</li>
+        </ul>
       </div>
-    );
+    )
   }
 }
-
-export default App;
