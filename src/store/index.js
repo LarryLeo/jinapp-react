abd62dd9857fc5055c4f7905648d234414262957
LarryLeo <esharemore@gmail.com>
2019-02-22 21:54:31 +0800

    配置redux-devtool，引用redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/index'
+import thunk from 'redux-thunk'
+import Immutable from 'immutable'
 
-const store = createStore(rootReducer)
+// Note that preloadedState(initialState) argument is optional in Redux' createStore
+const initialState = Immutable.Map()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
