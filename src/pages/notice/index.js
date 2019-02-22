4c5eb4d42d83668a9094dfffc2f842ad2873591c
LarryLeo <esharemore@gmail.com>
2019-02-23 00:25:06 +0800

    修复导航栏高度莫名缩小的bug

--- src/pages/notice/index.js
+++ src/pages/notice/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default class Notice extends Component {
   render() {
     return (
-      <div style={{backgroundColor: 'yellow', height: '100%'}}>
+      <div style={{backgroundColor: 'yellow',height: '100%'}}>
         政策宣传
         <p>
           <Link to='/notice/detail'>跳转至详情</Link>
