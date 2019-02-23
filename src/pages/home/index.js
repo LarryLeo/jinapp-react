478d84c9dc30367ee188c5b5431654f7e46285f0
LarryLeo <esharemore@gmail.com>
2019-02-23 18:41:53 +0800

    首页视觉渲染完成

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,103 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Flex } from "antd-mobile";
+
+import Swiper from "swiper/dist/js/swiper.js";
+import "swiper/dist/css/swiper.min.css";
+
+import slideImage from "../../assets/images/slider.jpg";
+import iconChat from '../../assets/icons/chat.png'
+import iconConsult from '../../assets/icons/consult.png'
+import iconGuide from '../../assets/icons/guide.png'
+import iconPolicy from '../../assets/icons/policy.png'
+import iconSuggestion from '../../assets/icons/suggestion.png'
+import bgImgWeather from '../../assets/images/weather.jpg'
+
 import { HomePageWrapper } from "./style";
 
 export default class Home extends Component {
+  state = {
+    slideImages: [slideImage, slideImage, slideImage]
+  };
   componentDidMount() {
-    console.log("首页挂载");
+    var mySwiper = new Swiper(".swiper-container", {
+      loop: true, //循环
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true // 允许点击跳转
+      }
+    });
   }
   render() {
     return (
       <HomePageWrapper>
-        <Link
+        <section className="swiper-container">
+          <div className="swiper-wrapper">
+            {this.state.slideImages.map((slide, index) => (
+              <div className="swiper-slide" key={index}>
+                <img
+                  className="slideImg"
+                  style={{ width: "100%" }}
+                  src={slide}
+                  alt="slide"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="swiper-pagination" />
+        </section>
+        {/* 功能导航  */}
+        <section className='menuGrid'>
+          <Flex wrap='wrap' justify='between'>
+            <div className='menuItem'>
+              <Flex justify='center'>
+                <img className='menuIcon' src={iconPolicy} alt=""/>
+                <span>政策宣传</span>
+              </Flex>
+            </div>
+            <div className='menuItem' style={{backgroundColor: '#ffa800'}}>
+              <Flex justify='center'>
+                <img className='menuIcon' src={iconGuide} alt=""/>
+                <span>办事指南</span>
+              </Flex>
+            </div>
+            <div className='menuItem' style={{backgroundColor: '#ff5555'}}>
+              <Flex justify='center'>
+                <img className='menuIcon' src={iconSuggestion} alt=""/>
+                <span>意见建议</span>
+              </Flex>
+            </div>
+            <div className='menuItem' style={{backgroundColor: '#00c9db'}}>
+              <Flex justify='center'>
+                <img className='menuIcon' src={iconConsult} alt=""/>
+                <span>求助咨询</span>
+              </Flex>
+            </div>
+            <div className='menuItem' style={{backgroundColor: '#007aff'}}>
+              <Flex justify='center'>
+                <img className='menuIcon' src={iconChat} alt=""/>
+                <span>警企互联</span>
+              </Flex>
+            </div>
+          </Flex>
+        </section>
+        {/* 咨询人数 */}
+        <section className='consultInfo'>
+          <Flex justify='between'>
+            <span style={{fontSize: '16px'}}>152人</span>
+            <a href="">立即咨询</a>
+          </Flex>
+          <p style={{marginTop: '20px', color: '#9ed5ff'}}>已在警企e通咨询</p>
+        </section>
+        {/* 天气 */}
+        <section className='weather'>
+          <img src={bgImgWeather} alt="weather" className='weatherBg' />
+          <div className='weatherText'>
+            <p className='temp'>9~11℃</p>
+            <p className='text'>今日天气：小雨</p>
+          </div>
+        </section>
+        {/* <Link
           to={{
             pathname: "/notice",
             state: {
@@ -19,7 +106,7 @@ export default class Home extends Component {
           }}
         >
           <p>跳转至政策宣传</p>
-        </Link>
+        </Link> */}
       </HomePageWrapper>
     );
   }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -3,5 +3,63 @@ import styled from 'styled-components'
 export const HomePageWrapper = styled.div`
   background-color: #fff;
   height: 100%;
-
+  padding: 5px 15px;
+  .swiper-container {
+    height: 150px;
+  }
+  /* 功能导航 */
+  .menuGrid {
+    margin: 15px 0;
+  }
+  .menuItem {
+    margin-bottom: 10px;
+    background-color: #389fff;
+    border-radius: 8px;
+    padding: 5;
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    width: 48%;
+    height: 78px;
+    line-height: 78px;
+  }
+  .menuIcon {
+    margin-right: 10px;
+    width: 23px;
+    height: 29px;
+  }
+  /* 咨询人数 */
+  .consultInfo {
+    padding: 20px;
+    margin: 10px 0;
+    background-color: #0093ff;
+    border-radius: 8px;
+    color: #fff;
+    height: 100px;
+    a {
+      color: #fff;
+      font-size: 16px;
+    }
+  }
+  /* 天气 */
+  .weather {
+    position: relative;
+    height: 110px;
+    overflow: hidden;
+    border-radius: 8px;
+    color: #fff;
+  }
+  .weatherBg {
+    width: 100%;
+  }
+  .weatherText {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+  .temp {
+    margin-bottom: 20px;
+    font-size: 20px;
+    font-weight: bold;
+  }
 `
