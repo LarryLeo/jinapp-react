df68208a3fe94ea4411adab9ab879e045f56bd45
LarryLeo <esharemore@gmail.com>
2019-02-25 20:57:45 +0800

    建议咨询条件渲染实现

--- src/pages/make/index.js
+++ src/pages/make/index.js
@@ -1,33 +1,140 @@
-import React, { Component } from 'react'
-import { MakeWrapper } from './style'
-import { Flex } from 'antd-mobile'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { MakeWrapper } from "./style";
+import { Flex, Picker, Button } from "antd-mobile";
+import { Link } from "react-router-dom";
 
-export default class Make extends Component {
-  renderHistoryJump = () => {
-    const { params:{type} } = this.props.match
-    switch(type) {
-      case 'suggestion':
-        return <Link to='/history'>
-          <span>意见历史</span>
-        </Link>
-      case 'consultation':
-        return <span>咨询历史</span>
-      default:
-        return <span>意见历史</span>
-    }
-
+const mockData = [
+  {
+    label: "AA",
+    value: "aa"
+  },
+  {
+    label: "公安",
+    value: "14"
+  },
+  {
+    label: "AA",
+    value: "aa"
+  },
+  {
+    label: "AA",
+    value: "aa"
   }
+];
+const PickerChildren = props => (
+  <div onClick={props.onClick} className="pickerChildren">
+    <div className="inlineWrapper">
+      <div className="pickerKey">{props.children}</div>
+      <div className="pickerVal">{props.extra}</div>
+    </div>
+  </div>
+);
+export default class Make extends Component {
+  state = {
+    currentPage: ""
+  };
+  renderHistoryJump = () => {
+    switch (this.state.currentPage) {
+      case "suggestion":
+        return (
+          <Link
+            to={{
+              pathname: "/my/history/suggestions",
+              state: {
+                title: "我的历史"
+              }
+            }}
+          >
+            <span>意见历史</span>
+          </Link>
+        );
+      case "consultation":
+        return (
+          <Link
+            to={{
+              pathname: "/my/history/consultations",
+              state: {
+                title: "我的历史"
+              }
+            }}
+          >
+            <span>咨询历史</span>
+          </Link>
+        );
+      default:
+        return (
+          <Link
+            to={{
+              pathname: "/my/history/suggestions",
+              state: {
+                title: "我的历史"
+              }
+            }}
+          >
+            <span>意见历史</span>
+          </Link>
+        );
+    }
+  };
   componentDidMount() {
-
+    const {
+      params: { type }
+    } = this.props.match;
+    this.setState({
+      currentPage: type
+    });
   }
   render() {
     return (
-      <MakeWrapper>
-        <Flex>
+      <MakeWrapper currentPage={this.state.currentPage}>
+        <Flex justify="end" className="historyJump">
           {this.renderHistoryJump()}
         </Flex>
+        <div className="picker">
+          <Picker
+            data={mockData}
+            value={["14"]}
+            cols={1}
+            extra="请选择意见对象"
+          >
+            <PickerChildren>
+              {this.state.currentPage === "suggestion" ? "意见建议" : "咨询"}
+              对象
+            </PickerChildren>
+          </Picker>
+        </div>
+        <div className="picker consultItemPicker">
+          <Picker
+            data={mockData}
+            value={["14"]}
+            cols={1}
+            extra="请选择意见对象"
+          >
+            <PickerChildren>咨询事项</PickerChildren>
+          </Picker>
+        </div>
+        <Flex className="subject">
+          <span className="key">
+            {this.state.currentPage === "suggestion" ? "建议" : "咨询"}主题
+          </span>
+          <input
+            maxLength="20"
+            className="value"
+            type="text"
+            placeholder="请输入主题"
+          />
+        </Flex>
+        <textarea
+          placeholder="请输入内容"
+          className="textArea"
+          name=""
+          id=""
+          rows="10"
+        />
+        <div style={{ marginTop: 20, padding: "0 15px" }}>
+          <Button type="primary">提交</Button>
+        </div>
       </MakeWrapper>
-    )
+    );
   }
 }
--- src/pages/make/style.js
+++ src/pages/make/style.js
@@ -1,5 +1,71 @@
 import styled from 'styled-components'
 
 export const MakeWrapper = styled.div`
-
+  color: #808080;
+  font-size: 16px;
+  .historyJump {
+    padding: 10px;
+    span {
+      font-size: 14px;
+      color: #8f8f94;
+    }
+  }
+  .picker {
+    margin-bottom: 10px;
+  }
+  .pickerChildren {
+    background-color: #fff;
+    padding: 5px 15px;
+    .inlineWrapper {
+      display: flex;
+      height: 45px;
+      line-height: 45px;
+      font-size: 16;
+      .pickerKey {
+        flex: 1;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      .pickerVal {
+        text-align: right;
+        color: #888;
+      }
+    }
+  }
+  .consultItemPicker {
+    display: ${props => props.currentPage === 'consultation' ? 'block' : 'none'}
+  }
+  .subject {
+    box-sizing: border-box;
+    position: relative;
+    background-color: #fff;
+    padding: 20px 15px;
+    margin-top: 10px;
+    &::after {
+      content: '';
+      display: block;
+      background-color: #d1d1d1;
+      height: 1px;
+      width: calc(100%  - 30px);
+      position: absolute;
+      bottom: 0;
+      left: 15px;
+    }
+  }
+  .key {
+    margin-right: 30px;
+  }
+  .value {
+    flex: 1;
+    border: none;
+    text-align: right;
+  }
+  .textArea {
+    box-sizing: border-box;
+    background-color: #fff;
+    width: 100%;
+    border: none;
+    padding: 15px;
+  }
 `
