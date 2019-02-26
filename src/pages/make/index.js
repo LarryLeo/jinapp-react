886eebebfc0f5c21326b14329da9488eb204bcbc
LarryLeo <esharemore@gmail.com>
2019-02-26 09:55:45 +0800

    添加提交逻辑

--- src/pages/make/index.js
+++ src/pages/make/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { MakeWrapper } from "./style";
-import { Flex, Picker, Button } from "antd-mobile";
+import { Flex, Picker, Button, Toast } from "antd-mobile";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchUnitList, fetchConsultSubject } from "../../actions/index";
+import { requestPost } from '../../utils/utils'
 
 const PickerChildren = props => (
+  // 自定义pickerChildren
   <div onClick={props.onClick} className="pickerChildren">
     <div className="inlineWrapper">
       <div className="pickerKey">{props.children}</div>
@@ -19,7 +21,10 @@ class Make extends Component {
     unitPickerData: [],
     selectedUnit: "",
     consultSubjectPickerData: [{ label: "默认", value: -1 }],
-    selectedConsultSubject: ""
+    selectedConsultSubject: "",
+    title: '',
+    content: '',
+
   };
   renderHistoryJump = () => {
     switch (this.state.currentPage) {
@@ -70,6 +75,33 @@ class Make extends Component {
     if(this.state.currentPage === 'suggestion') return
     return this.props.fetchConsultSubject(val);
   };
+  submit = async() => {
+    // 检查信息完整性
+    if(!this.state.selectedUnit.length) return Toast.show('请选择单位')
+    if(this.state.currentPage === 'consultation' && !this.state.selectedConsultSubject.length) return Toast.show('请选择咨询事项')
+    if(!this.state.title.length) return Toast.show('请填写标题')
+    if(!this.state.title.length) return Toast.show('请填写内容')
+
+    let userCredential = JSON.parse(localStorage.getItem('userCredential'))
+    let apiUrl = this.state.currentPage === 'suggestion' ? '/app/v1/suggestion/addSuggestion' : '/app/v1/consult/addConsult'
+    let data = {
+      unit_id: this.state.selectUnitId,
+      title: this.state.title,
+      content: this.state.content,
+      member_id: userCredential.member_id,
+      member_token: userCredential.member_token
+    }
+    let res = await requestPost({
+      apiUrl: apiUrl,
+      data: this.state.currentPage === 'suggestion' ? data : {...data, consult_subject_id: this.state.selectedConsultSubject}
+    })
+    res.success && Toast.success('发送成功')
+    !res.success && Toast.fail('发送失败')
+    this.setState({
+      content: '',
+      title: ''
+    })
+  }
 
   componentDidMount() {
     // redux中未缓存单位列表即请求数据
@@ -155,17 +187,21 @@ class Make extends Component {
             className="value"
             type="text"
             placeholder="请输入主题"
+            value={this.state.title}
+            onChange={(e) => this.setState({title: e.target.value})}
           />
         </Flex>
         <textarea
           placeholder="请输入内容"
+          value={this.state.content}
+          onChange={(e) => this.setState({content: e.target.value})}
           className="textArea"
           name=""
           id=""
           rows="10"
         />
         <div style={{ marginTop: 20, padding: "0 15px" }}>
-          <Button type="primary">提交</Button>
+          <Button type="primary" onClick={() => this.submit()}>提交</Button>
         </div>
       </MakeWrapper>
     );
