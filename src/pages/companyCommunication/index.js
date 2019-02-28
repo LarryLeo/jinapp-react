8d0e8c19dcf89f4cca6d66cfba44eff37c4f6af3
LarryLeo <esharemore@gmail.com>
2019-02-28 19:06:25 +0800

    上传单张图片实现

--- src/pages/companyCommunication/index.js
+++ src/pages/companyCommunication/index.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react'
 import { Tabs, Flex, Picker, Button, Toast } from 'antd-mobile'
-import ImageUploader from 'react-images-upload'
+import * as qiniu from 'qiniu-js'
 import { FiUpload } from 'react-icons/fi'
 import { Communication } from './style'
+import { requestGet } from '../../utils/utils'
 
 const reader = new FileReader()
 export default class CompanyCommunication extends Component {
+  state = {
+    selectedImage: '',
+    uploadedImage: '',
+    uploadToken: ''
+  }
+
+// 测试七牛上传
+  qinUpload = () => {
+    // 对必要参数进行配置
+    let file = this.state.selectedImage
+    let key = `jinapp/${file.name}`
+    let token = this.state.uploadToken
+    let config = {
+      region: qiniu.region.z2,
+    }
+    let putExtra = {
+      fname: file.name,
+      params: {},
+      mimeType: ["image/png", "image/jpeg", "image/gif"] || null
+};
+    let observable = qiniu.upload(file, key, token, putExtra, config)
+
+    observable.subscribe({
+      next: res => console.log(res),
+      error: err => console.log(err),
+      complete: res => console.log(res)
+    }) // 上传开始
+      }
+
   uploadImage = (e) => {
     console.log(e.target.files)
+    this.setState({selectedImage: e.target.files[0]})
     reader.readAsDataURL(e.target.files[0])
     reader.onloadend = () => {
       console.log(reader.result)
     }
+  }
+  getUploadToken = async() => {
+    let res = await requestGet({apiUrl: '/app/v1/file/uploadToken', data: {has_key: 0}})
+    res.success && this.setState({uploadToken: res.data.upload_token})
+  }
+  componentDidMount() {
+    this.getUploadToken()
   }
   render() {
     return (
@@ -48,7 +86,7 @@ export default class CompanyCommunication extends Component {
                 </div>
               </Flex>
               <div className='submit'>
-              <Button type='primary'>提交</Button>
+              <Button type='primary' onClick={() => this.qinUpload()}>提交</Button>
             </div>
             </section>
             <section className='chat'>
