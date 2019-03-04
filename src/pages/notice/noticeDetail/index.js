52875359e130cadd809b194416a96b11d4b8d291
LarryLeo <esharemore@gmail.com>
2019-03-04 21:36:38 +0800

    修复宣传详情不滚动的bug

--- src/pages/notice/noticeDetail/index.js
+++ src/pages/notice/noticeDetail/index.js
@@ -9,7 +9,7 @@ export default class NoticeDetail extends Component {
           <h2 style={{fontSize: '18px', fontWeight: 'bold'}}>{data.title}</h2>
           <div style={{display: 'flex', justifyContent: 'flex-end', padding: '20px 0', borderBottom: '1px solid #d1d1d1'}}>{data.updated_at}</div>
         </section>
-        <div style={{lineHeight: '1.5rem', padding: '0 15px',color: '#3a3a3a'}} dangerouslySetInnerHTML={{
+        <div style={{lineHeight: '1.5rem', padding: '0 15px',color: '#3a3a3a', height: '100%', overflow: 'auto'}} dangerouslySetInnerHTML={{
           __html: data.body
         }}>
         </div>
